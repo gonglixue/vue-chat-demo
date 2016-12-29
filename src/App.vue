5dcb34cc6ab27c190c5f7fcb3f3378a569cfa8ac
gonglixue <724351282@qq.com>
2016-12-29 17:29:58 +0800

    添加app组件

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script>
+module.exports={
+        created:function(){
+            console.log('created')
+        }
+    }
+</script>
+
+<template>
+    <div id="app">
+        <p>hello app template</p>
+    </div>
+</template>
+
+<style lang="less" scoped>
+    #app{
+        p{
+            color:red;
+        }
+    }
+</style>
--- src/main.js
+++ src/main.js
@@ -2,10 +2,14 @@
  * Created by chenjun on 2016/12/29.
  */
 import Vue from 'vue';
+import App from './App';
 
 var app = new Vue({
     el:'#body',
     data:{
         message:'hello vue chat'
+    },
+    components:{
+        'app':App
     }
 })
