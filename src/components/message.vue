5088475b50e98888c1495ce8a7a6f393b21e923b
gonglixue <724351282@qq.com>
2016-12-29 17:50:25 +0800

    message组件模板

---
+++ src/components/message.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="message" v-scroll-bottom="session.messages">
+        <ul v-if="session">
+            <li v-for="item in session.messages">
+                <p class="item">
+                    <span>{{item.date | time}}</span>
+                </p>
+                <div class="main" v-bind:class="{self: item.self}">
+                    <img class="avatar" width="30" height="30" v-bind:src="item.self? user.img : session.user.img"/>
+                    <div class="text">{{ item.content }}</div>
+                </div>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<style lang="less" scoped>
+    .message{
+        
+    }
+</style>
